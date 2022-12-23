45383b6716256198317a488051f7f45a9a7862fa
NiluhNiaDevi <109826288+NiluhNiaDevi@users.noreply.github.com>
2022-12-23 08:52:03 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,12 @@
+import os
+import time
+import pandas as pd
+import numpy as np
+import warnings
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+
+from sklearn.preprocessing import StandardScaler 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import classification_report
